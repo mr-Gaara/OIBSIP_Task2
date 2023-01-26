20c9e10082039de144b87e1f87d0197b9c954fb9
mr-Gaara <120037919+mr-Gaara@users.noreply.github.com>
2023-01-26 11:54:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+    strings: ["Developer", "Blogger", "Photographer", "Content Creator"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+});
